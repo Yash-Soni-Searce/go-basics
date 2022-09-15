ff02eb55e27ee58234ab7f9f8874b38d516362d4
Yash Soni <yash.soni@searce.com>
2022-09-15 15:50:38 +0530

    Added assignment

---
+++ assignment/assignment.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+type shape interface {
+	getArea() float64
+}
+
+type square struct {
+	side float64
+}
+
+type triangle struct {
+	base   float64
+	height float64
+}
+
+func (s square) getArea() float64 {
+	return s.side * s.side
+}
+
+func (t triangle) getArea() float64 {
+	return 0.5 * t.base * t.height
+}
+
+func printArea(s shape) {
+	fmt.Println(s.getArea())
+}
+
+func main() {
+	sq := square{side: 60}
+	tr := triangle{base: 60, height: 60}
+	printArea(sq)
+	fmt.Println("///")
+	printArea(tr)
+	fmt.Println("///")
+}
