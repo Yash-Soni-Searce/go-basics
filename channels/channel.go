dfa9f728579d1243562839e7be3578b31d83d0ff
Yash Soni <yash.soni@searce.com>
2022-09-19 15:36:39 +0530

    Channel beginning code

---
+++ channels/channel.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	links := []string {
+		"https://searce.udemy.com/course/go-the-complete-developers-guide/learn/lecture/7797426#questions",
+		"https://mail.google.com/mail/u/0/#search/mike+gouline/FMfcgzGqPphQGzdBBKjWMnThbTJZggKn",
+	}
+
+	for _,link := range links {
+		check(link)
+	}
+}
+
+func check(link string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "is down")
+	}
+		fmt.Println(link, "is up")
+}
