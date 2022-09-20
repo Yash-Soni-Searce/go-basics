d6e9998493560ee181bb047a78a062788409244e
Yash Soni <yash.soni@searce.com>
2022-09-20 15:28:46 +0530

    Channel code changes

--- routine/main.go
+++ routine/main.go
@@ -9,17 +9,23 @@ func main() {
 	links := []string {
 		"https://searce.udemy.com/course/go-the-complete-developers-guide/learn/lecture/7797426#questions",
 		"https://mail.google.com/mail/u/0/#search/mike+gouline/FMfcgzGqPphQGzdBBKjWMnThbTJZggKn",
+		"https://notog.org",
 	}
 
+	c := make(chan string)
+
 	for _,link := range links {
-		go check(link)
+		go check(link, c)
+		fmt.Println(<- c)
 	}
 }
 
-func check(link string) {
+func check(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "is down")
-	}
+	} else {
 		fmt.Println(link, "is up")
+		c <- "Yes it is up"
+	}
 }
